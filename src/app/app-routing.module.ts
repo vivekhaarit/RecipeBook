d184dd3b7f1fb5dedb092b2376a4270d9eb17de3
Vivek Kumar <vivekhaarit@gmail.com>
2022-07-03 23:36:02 +0530

    error route added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './error/error.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
@@ -9,10 +11,14 @@ const routes: Routes = [
   {path:'', redirectTo: "/recipes", pathMatch:"full"},
   {path:'recipes',component: RecipesComponent, children:[
     {path: '',component:RecipeStartComponent},
+    {path:'new', component: RecipeEditComponent},
     {path:':id', component: RecipeDetailComponent},
+    {path:':id/edit', component: RecipeEditComponent}
     
   ]},
-  {path:'shopping-list',component: ShoppingListComponent}
+  {path:'shopping-list',component: ShoppingListComponent},
+  {path:'error',component:ErrorComponent},
+  {path:'**', redirectTo:"/error"}
 ];
 
 @NgModule({

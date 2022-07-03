8347b49532806685a5004535668cd6f2e0443cf1
Vivek Kumar <vivekhaarit@gmail.com>
2022-07-03 20:11:51 +0530

    removing styles of shopping list

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -6,7 +6,6 @@ import { RecipeService } from './recipe.service';
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
-  styleUrls: ['./recipes.component.css'],
   providers:[RecipeService]
 })
 export class RecipesComponent implements OnInit {
